a107422843b4bdbde7bbcc746e891dd862ee7c1d
Ilysha21rus <Ilya-stepanov-0@mail.ru>
2025-10-06 02:53:07 +0300

    добавил фигуры в коллекцию

--- SteamWorkshopExplorer/ViewModels/MainWindowViewModel.cs
+++ SteamWorkshopExplorer/ViewModels/MainWindowViewModel.cs
@@ -5,16 +5,16 @@ using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 
 namespace SteamWorkshopExplorer.ViewModels;
-{ 
-    public class MainWindowModel
+{
+    public class MainWindowViewModel
     {
         public ObservableCollection<ShapeModel> Shapes { get; set; } = new();
-        
+
         public ICommand AddBezierCommand { get; }
         public ICommand AddEllipseCommand { get; }
         public ICommand ClearCommand { get; }
 
-        public MainViewModel()
+        public MainWindowViewModel()
         {
             AddEllipseCommand = new RelayCommand(_ => AddEllipse(50, 50));
             AddBezierCommand = new RelayCommand(_ =>
@@ -29,3 +29,17 @@ namespace SteamWorkshopExplorer.ViewModels;
 
             ClearCommand = new RelayCommand(_ => Shapes.Clear());
         }
+
+        public void AddEllipse(double x, double y)
+        {
+            Shapes.Add(new EllipseModel { X = x, Y = y });
+        }
+
+        public void AddBezier(params Point[] points)
+        {
+            var bez = new BezierModel();
+            foreach (var point in points) bez.ControlPoints.Add(point);
+            Shapes.Add(bez);
+        }
+    }
+}
