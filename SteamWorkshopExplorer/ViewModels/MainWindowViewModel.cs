a07db88a635ac37e51a290df2a124762930a9e7c
Ilysha21rus <Ilya-stepanov-0@mail.ru>
2025-10-06 02:42:14 +0300

    добавил команды фигур и очистки

--- SteamWorkshopExplorer/ViewModels/MainWindowViewModel.cs
+++ SteamWorkshopExplorer/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using SteamWorkshopExplorer.Models;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SteamWorkshopExplorer.ViewModels;
 { 
@@ -12,4 +13,19 @@ namespace SteamWorkshopExplorer.ViewModels;
         public ICommand AddBezierCommand { get; }
         public ICommand AddEllipseCommand { get; }
         public ICommand ClearCommand { get; }
-        
+
+        public MainViewModel()
+        {
+            AddEllipseCommand = new RelayCommand(_ => AddEllipse(50, 50));
+            AddBezierCommand = new RelayCommand(_ =>
+            {
+                AddBezier(
+                    new Point(50, 200),
+                    new Point(150, 100),
+                    new Point(250, 100),
+                    new Point(350, 200)
+                );
+            });
+
+            ClearCommand = new RelayCommand(_ => Shapes.Clear());
+        }
