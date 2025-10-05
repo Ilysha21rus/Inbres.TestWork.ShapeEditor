5be459ab94bc79eea26018b117690d557d935099
Ilysha21rus <Ilya-stepanov-0@mail.ru>
2025-10-06 01:47:34 +0300

    й

--- SteamWorkshopExplorer/Models/ShapeModel.cs
+++ SteamWorkshopExplorer/Models/ShapeModel.cs
@@ -1,11 +1,11 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 
 namespace SteamWorkshopExplorer.Models
 {
     public abstract class ShapeModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged(string name) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
