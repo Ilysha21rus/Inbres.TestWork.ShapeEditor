d7b82f6b057cb2ae85d4560216fbe76c183625ab
Ilysha21rus <Ilya-stepanov-0@mail.ru>
2025-10-07 02:37:12 +0300

    переделал MainWindowViewModel для CommunityToolkit

--- SteamWorkshopExplorer/ViewModels/MainWindowViewModel.cs
+++ SteamWorkshopExplorer/ViewModels/MainWindowViewModel.cs
@@ -3,53 +3,32 @@ using SteamWorkshopExplorer.Models;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace SteamWorkshopExplorer.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ObservableObject
     {
         public ObservableCollection<ShapeModel> Shapes { get; set; } = new();
 
-        public ICommand AddBezierCommand { get; }
-        public ICommand AddEllipseCommand { get; }
-        public ICommand ClearCommand { get; }
-        public ICommand SaveCommand { get; }
-
-        public MainWindowViewModel()
+        private void AddEllipse()
         {
-            AddEllipseCommand = new RelayCommand(_ => AddEllipse(50, 50));
-            AddBezierCommand = new RelayCommand(_ =>
-            {
-                AddBezier(
-                    new Point(50, 200),
-                    new Point(150, 100),
-                    new Point(250, 100),
-                    new Point(350, 200)
-                );
-            });
-
-            ClearCommand = new RelayCommand(_ => Shapes.Clear());
+            Shapes.Add(new EllipseModel { X = 100, Y = 100, Width = 100, Height = 100 });
         }
 
-        public void AddEllipse(double x, double y)
-        {
-            Shapes.Add(new EllipseModel { X = x, Y = y });
-        }
-
-        public void AddBezier(params Point[] points)
+        private void AddBezier()
         {
             var bez = new BezierModel();
-            foreach (var point in points) bez.ControlPoints.Add(point);
+            bez.ControlPoints.Add(new Point(50, 200));
+            bez.ControlPoints.Add(new Point(150, 100));
+            bez.ControlPoints.Add(new Point(250, 100));
+            bez.ControlPoints.Add(new Point(350, 200));
             Shapes.Add(bez);
         }
-    }
 
-    public class RelayCommand : ICommand
-    {
-        private readonly System.Action<object> _execute;
-        public event System.EventHandler? CanExecuteChanged;
-        public RelayCommand(System.Action<object> execute) => _execute = execute;
-        public bool CanExecute(object? parameter) => true;
-        public void Execute(object? parameter) => _execute(parameter);
+        private void Clear()
+        {
+            Shapes.Clear();
+        }
     }
-}
+}f
