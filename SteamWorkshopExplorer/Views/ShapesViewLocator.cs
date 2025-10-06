cd9233ff3f4a9308d4b197d607b4b3b7e7244ef0
Andreev-Da <da18.06.03@yandex.ru>
2025-10-06 12:14:34 +0300

    Fixed application and added Todos

---
+++ SteamWorkshopExplorer/Views/ShapesViewLocator.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using ReactiveUI;
+
+namespace SteamWorkshopExplorer.Views;
+
+public class ShapesViewLocator : IDataTemplate
+{
+    public Control? Build(object? param)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Match(object? data)
+    {
+        throw new System.NotImplementedException();
+    }
+}
