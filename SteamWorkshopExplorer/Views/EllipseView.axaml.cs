955fced7d5f1e3ee0001626be2fe84388465585b
Ilysha21rus <Ilya-stepanov-0@mail.ru>
2025-10-07 02:51:14 +0300

    привязал свойство модели овала

---
+++ SteamWorkshopExplorer/Views/EllipseView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SteamWorkshopExplorer.Views;
+
+public partial class EllipseView : UserControl
+{
+    public EllipseView()
+    {
+        InitializeComponent();
+    }
+}
